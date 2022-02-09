dad4b0b3e2df13b8a8d58472bad534611e0391e2
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2022-02-09 13:57:11 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 const api="https://tribe.api.fdnd.nl/v1/member";
 
-async function(){
+async function getData(){
 
-    const response= await fetch("")
+    const response= await fetch(api)
+    const data =await response.json();
+    console.log(data[0].contact)
+    // document.querySelector('article h4').innerHTML=data
+    // printData(data)
+
 }
+getData()
